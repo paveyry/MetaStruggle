63cc219a2a1c039b4a3e1158b33bec9c673658e9
hasB4K <mathis.felardos@gmail.com>
2013-04-30 10:13:02 +0200

    nothing important

--- MetaStruggle/MS/GameClient/Renderable/Scene/HUD.cs
+++ MetaStruggle/MS/GameClient/Renderable/Scene/HUD.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using GameClient.Characters;
 using GameClient.Global;
-using GameClient.Renderable._3D;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -18,8 +15,8 @@ namespace GameClient.Renderable.Scene
         public HUD()
         {
             _players = new List<Character>();
-            _intervalSize.X = (int)Math.Floor(RessourceProvider.Fonts["HUD"].MeasureString(300 + "%").X) + 1;
-            _intervalSize.Y = (int)Math.Floor(RessourceProvider.Fonts["HUD"].MeasureString(300 + "%").Y) + 1;
+            _intervalSize.X = (int)Math.Floor(RessourceProvider.Fonts["HUD"].MeasureString(999 + "%").X) + 1;
+            _intervalSize.Y = (int)Math.Floor(RessourceProvider.Fonts["HUD"].MeasureString(999 + "%").Y) + 1;
         }
 
         public void AddCharacter(Character newChar)
@@ -35,26 +32,23 @@ namespace GameClient.Renderable.Scene
             spriteBatch.Begin();
             foreach (var character in _players)
             {
-                spriteBatch.Draw(character.Face,
-                    new Rectangle(x, addCoordonate, character.Face.Width / divCoef, character.Face.Height / divCoef), Color.White);
+                int width = character.Face.Width / divCoef, height = character.Face.Height / divCoef;
+
+                spriteBatch.Draw(character.Face, new Rectangle(x, addCoordonate, width, height), Color.White);
 
                 if (character.IsDead)
                     spriteBatch.DrawString(RessourceProvider.Fonts["HUDlittle"],
                                            Lang.Language.GetString("respawn") +
-                                           ((int)(5 - (DateTime.Now - character.DeathDate).TotalSeconds)).ToString(),
-                                           new Vector2(x + addCoordonate, character.Face.Height / divCoef + addCoordonate), Color.DarkOrange);
+                                           ((int) (5 - (DateTime.Now - character.DeathDate).TotalSeconds)),
+                                           new Vector2(x + addCoordonate, height + addCoordonate), Color.DarkOrange);
                 else
                     spriteBatch.DrawString(RessourceProvider.Fonts["HUDlittle"],
                                            character.PlayerName,
-                                           new Vector2(x + addCoordonate, character.Face.Height / divCoef + addCoordonate), Color.White);
-
-                x += character.Face.Width / divCoef;
-
+                                           new Vector2(x + addCoordonate, height + addCoordonate), Color.White);
+                x += width;
                 spriteBatch.DrawString(RessourceProvider.Fonts["HUD"], character.Damages + "%",
-                    new Vector2(x, character.Face.Height / divCoef - (int)_intervalSize.Y), Color.White);
-
+                                       new Vector2(x, height - (int) _intervalSize.Y), Color.White);
                 x += (int)_intervalSize.X;
-
             }
             spriteBatch.End();
             spriteBatch.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
