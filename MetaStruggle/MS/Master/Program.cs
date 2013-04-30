ce3a1aa155a4d185bf0513419021cb7eb7a46992
neodyblue <neodyblue@gmail.com>
2013-04-30 04:57:53 +0200

    Ajout Master

---
+++ MetaStruggle/MS/Master/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Network;
+using Network.Packet.Packets.DatasTypes;
+
+namespace Master
+{
+    class Program
+    {
+        static readonly List<MasterServerDatas> Servers = new List<MasterServerDatas>();
+        private static Server _s;
+        static void Main(string[] args)
+        {
+            PrintTitle();
+
+            var em = new EventManager();
+            em.Register("Network.Master.AddServer", AddServer);
+            em.Register("Network.Master.RemoveServer", RemoveServer);
+            em.Register("Network.Master.ServerListRequest", SendServerList);
+
+            InitServer(em, 5555);
+
+            while (true)
+                switch (Console.ReadLine())
+                {
+                    case "quit":
+                        _s.Stop();
+                        Environment.Exit(0);
+                        break;
+                }
+        }
+
+        static void InitServer(EventManager ev, int port)
+        {
+            var p = new Parser();
+            p.PacketNotFound += (pck) => Console.WriteLine("Packet inconnu [ID={0}]", pck.Header.ID);
+            _s = new Server(ev, p.Parse);
+            _s.ClientConnected += (c) => Console.WriteLine("Client connected [{0}]", c.ToString());
+            _s.ClientDisconnected += (c) => Console.WriteLine("Client disconnected [{0}]", c.ToString());
+            _s.Start(port);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Server UP !");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        static void PrintTitle()
+        {
+            Console.WriteLine("   _____    _________ ");
+            Console.WriteLine("  /     \\  /   _____/                                            ");
+            Console.WriteLine(" /  \\ /  \\ \\_____  \\                                             ");
+            Console.WriteLine("/    Y    \\/        \\                                            ");
+            Console.WriteLine("\\____|__  /_______  /                                            ");
+            Console.WriteLine("        \\/        \\/                                             ");
+            Console.WriteLine("   _____      _____    _________________________________________ ");
+            Console.WriteLine("  /     \\    /  _  \\  /   _____/\\__    ___/\\_   _____/\\______   \\");
+            Console.WriteLine(" /  \\ /  \\  /  /_\\  \\ \\_____  \\   |    |    |    __)_  |       _/");
+            Console.WriteLine("/    Y    \\/    |    \\/        \\  |    |    |        \\ |    |   \\");
+            Console.WriteLine("\\____|__  /\\____|__  /_______  /  |____|   /_______  / |____|_  /");
+            Console.WriteLine("        \\/         \\/        \\/                    \\/         \\/ ");
+            Console.WriteLine("  __________________________________   _________________________ ");
+            Console.WriteLine(" /   _____/\\_   _____/\\______   \\   \\ /   /\\_   _____/\\______   \\");
+            Console.WriteLine(" \\_____  \\  |    __)_  |       _/\\   Y   /  |    __)_  |       _/");
+            Console.WriteLine(" /        \\ |        \\ |    |   \\ \\     /   |        \\ |    |   \\");
+            Console.WriteLine("/_______  //_______  / |____|_  /  \\___/   /_______  / |____|_  /");
+            Console.WriteLine("        \\/         \\/         \\/                   \\/         \\/ ");
+        }
+
+        static void AddServer(object data)
+        {
+            var serverDatas = (MasterServerDatas) data;
+
+            lock (Servers)
+                if (Servers.All(e => e.IP != serverDatas.IP || e.Port != serverDatas.Port))
+                {
+                    Servers.Add(serverDatas);
+                    Console.WriteLine("[{0}:{1}] ajouté", serverDatas.IP, serverDatas.Port);
+                }
+        }
+
+        static void RemoveServer(object data)
+        {
+            var serverDatas = (MasterServerDatas)data;
+
+            lock (Servers)
+            {
+                if (!Servers.Any(e => e.IP == serverDatas.IP && e.Port == serverDatas.Port)) return;
+
+                Servers.RemoveAll(e => e.IP == serverDatas.IP && e.Port == serverDatas.Port);
+                Console.WriteLine("[{0}:{1}] supprimé", serverDatas.IP, serverDatas.Port);
+            }
+        }
+
+        static void SendServerList(object data)
+        {
+            new Network.Packet.Packets.MasterServerList().Pack(((Client) data).Writer, Servers);
+        }
+    }
+}
