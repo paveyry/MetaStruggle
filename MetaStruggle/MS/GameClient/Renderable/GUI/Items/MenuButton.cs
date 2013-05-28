e56050fb72e79207cfe5319f6feaef9096a8db11
hasB4K <mathis.felardos@gmail.com>
2013-05-29 01:42:38 +0200

    Systéme de ratio (avec une delegate, si on veut modif le truc)

--- MetaStruggle/MS/GameClient/Renderable/GUI/Items/MenuButton.cs
+++ MetaStruggle/MS/GameClient/Renderable/GUI/Items/MenuButton.cs
@@ -19,11 +19,13 @@ namespace GameClient.Renderable.GUI.Items
         }
 
         public delegate Texture2D TextureFunc(bool isSelect);
+        public delegate float RatioFunc();
 
         public string Id { get; set; }
         public NameFunc Text { get; set; }
         public bool IsSelect { get; set; }
         private TextureFunc Image { get; set; }
+        private RatioFunc Ratio { get; set; }
         private SpriteFont Font { get; set; }
         private Color ColorNormal { get; set; }
         private Color ColorSelected { get; set; }
@@ -40,7 +42,8 @@ namespace GameClient.Renderable.GUI.Items
         private Rectangle NormalRectangle { get; set; }
         private StatusMenuButton Status { get; set; }
 
-        public MenuButton(string id, Vector2 pos, StatusMenuButton status, bool abstractPos, NameFunc text, TextureFunc image, SpriteFont font, Color normal, Color selected, Event onClick, bool isDrawable = true)
+        public MenuButton(string id, Vector2 pos, StatusMenuButton status, bool abstractPos, NameFunc text, TextureFunc image,
+            RatioFunc ratioFunc,SpriteFont font, Color normal, Color selected, Event onClick, bool isDrawable = true)
             : base(new Rectangle((int)pos.X, (int)pos.Y, 0, 0),isDrawable)
         {
             Id = id;
@@ -48,15 +51,18 @@ namespace GameClient.Renderable.GUI.Items
             Status = status;
             Text = text;
             Image = image;
+            Ratio = ratioFunc;
             Font = font;
             ColorNormal = normal;
             ColorSelected = selected;
             OnClick = onClick;
             UpdateRectangles();
         }
-        public MenuButton(string id, Vector2 pos, StatusMenuButton status, bool abstractPos, SpriteFont font, Color normal, Color selected, Event onClick, bool isDrawable = true)
+        public MenuButton(string id, Vector2 pos, StatusMenuButton status, bool abstractPos, SpriteFont font,
+            Color normal, Color selected, Event onClick, bool isDrawable = true)
             : this(id, pos, status,abstractPos, () => GameEngine.LangCenter.GetString(id), 
-            (isSelect) => GameEngine.LangCenter.GetImage(id, !isSelect), font, normal, selected, onClick, isDrawable) { }
+            (isSelect) => GameEngine.LangCenter.GetImage(id, !isSelect),() => GameEngine.Config.ResolutionWidth/1920f
+            , font, normal, selected, onClick, isDrawable) { }
         public MenuButton(string id, Vector2 pos, SpriteFont font, Color normal, Color selected, Event onClick, bool isDrawable = true) 
             : this(id, pos, StatusMenuButton.None,true, font, normal, selected, onClick, isDrawable) { }
 
@@ -64,20 +70,21 @@ namespace GameClient.Renderable.GUI.Items
         {
             var image = Image.Invoke(status);
             var text = Font.MeasureString(Text.Invoke());
+            var ratio = Ratio.Invoke();
             switch (Status)
             {
                 case StatusMenuButton.Vertical:
                     return image != null
-                        ? new Rectangle((int)(MiddlePos.X - image.Width / 2f), (int)(MiddlePos.Y), image.Width, image.Height)
+                        ? new Rectangle((int)(MiddlePos.X - (image.Width*ratio) / 2f), (int)(MiddlePos.Y), (int)(image.Width * ratio), (int)(image.Height * ratio))
                         : new Rectangle((int)(MiddlePos.X - text.X / 2f), (int)(MiddlePos.Y), (int)text.X, (int)text.Y);
 
                 case StatusMenuButton.Horizontal:
                     return image != null
-                        ? new Rectangle((int)(MiddlePos.X), (int)(MiddlePos.Y - image.Height / 2f), image.Width, image.Height)
+                        ? new Rectangle((int)(MiddlePos.X), (int)(MiddlePos.Y - (image.Height*ratio) / 2f), (int)(image.Width * ratio), (int)(image.Height * ratio))
                         : new Rectangle((int)(MiddlePos.X), (int)(MiddlePos.Y - text.Y / 2f), (int)text.X, (int)text.Y);
                 default:
                     return image != null
-                        ? new Rectangle((int)(MiddlePos.X), (int)(MiddlePos.Y), image.Width, image.Height)
+                        ? new Rectangle((int)(MiddlePos.X), (int)(MiddlePos.Y), (int)(image.Width * ratio), (int)(image.Height * ratio))
                         : new Rectangle((int)(MiddlePos.X), (int)(MiddlePos.Y), (int)text.X, (int)text.Y);
             }
         }
