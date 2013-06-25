35ca7adcabe0b20221e2f85d369624017d21104d
Areswar <veyry_p@epita.fr>
2013-06-25 18:07:59 +0200

    Bouton menu principal sur l'écran de fin. Attention
    Attention, les particules restent.

--- MetaStruggle/MS/GameClient/Menus/MenuGameOver.cs
+++ MetaStruggle/MS/GameClient/Menus/MenuGameOver.cs
@@ -7,6 +7,8 @@ using GameClient.Global;
 using GameClient.Renderable.GUI;
 using GameClient.Renderable.GUI.Items;
 using GameClient.Renderable.GUI.Items.ListItems;
+using GameClient.Renderable.Layout;
+using GameClient.Renderable.Scene;
 using Microsoft.Xna.Framework;
 
 namespace GameClient.Menus
@@ -23,11 +25,26 @@ namespace GameClient.Menus
                 {
                     {"Rang",20},
                     {"Jouers",80}
-                },RessourceProvider.Fonts["MenuLittle"],Color.DarkOrange,Color.DarkOrange,"MSTheme" ));
-            Menu.Add("WinnerHead.Item", new ImageButton(characters.Peek().PlayerName,new Rectangle(40,10,characters.Peek().Face.Width/3,characters.Peek().Face.Height/3),
+                },RessourceProvider.Fonts["MenuLittle"],Color.White,Color.White,"MSTheme" ));
+            Menu.Add("WinnerHead.Item", new ImageButton(characters.Peek().PlayerName,new Rectangle(80,80,characters.Peek().Face.Width/3,characters.Peek().Face.Height/3),
                 characters.Peek().Face,RessourceProvider.Fonts["MenuLittle"]));
-
+            var buttons = new List<PartialButton>
+                              {
+                                  new PartialButton("Pause.ReturnMainMenu", ReturnMainMenu)
+                              };
+            Menu.Add("Buttons.Item", new ListButtons(new Vector2(85, 90), 20, buttons, RessourceProvider.Fonts["Menu"],
+                Color.White, Color.White, ListButtons.StatusListButtons.Vertical));
             return Menu;
+
+        }
+
+        void ReturnMainMenu()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameEngine.DisplayStack.Pop();
+            }
+            System.Threading.Thread.Sleep(200);
         }
 
         IEnumerable<string[]> CreateLines(IEnumerable<Character> characters)
